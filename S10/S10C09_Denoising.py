6f926930541b7dcde861116632698960c6f4ca54
Morteza Moradzadeh <47799003+mortezam037@users.noreply.github.com>
2022-06-29 20:13:35 +0430

    Update and rename S10C09.py to S10C09_Denoising.py

--- S10/S10C09_Denoising.py
+++ S10/S10C09_Denoising.py
@@ -1,10 +1,15 @@
 import cv2
 import matplotlib.pyplot as plt
 img = cv2.imread("Amirkola_City.bmp",0)
+
 SE = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
 #SE = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+
+# Denoising (Openning and Closing)
 imgO=cv2.morphologyEx(img, cv2.MORPH_OPEN, SE)
 imgDenois=cv2.morphologyEx(imgO, cv2.MORPH_CLOSE, SE)
+
+# Visualize
 images=[img,imgDenois]
 titles=['Original','Denoised']
 fig1 = plt.figure(figsize=(10, 10))
