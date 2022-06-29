d990aa33833b95a1e2b0a88b772745bb46f112c7
Morteza <47799003+mortezam037@users.noreply.github.com>
2022-06-29 16:32:28 +0430

    Create S10C05.py

---
+++ S10/S10C05.py
@@ -0,0 +1,14 @@
+import cv2
+import matplotlib.pyplot as plt
+img = cv2.imread("Test_C.BMP",0)
+SE = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
+img1 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, SE)
+images=[img,img1]
+titles=['Orginal','Closing']
+fig1 = plt.figure(figsize=(10, 10))
+for idx in range(2):
+    ax = fig1.add_subplot(1, 2, idx+1)
+    ax.imshow(images[idx],cmap='gray')
+    ax.set_axis_off()
+    ax.set_title(titles[idx])
+plt.show()
