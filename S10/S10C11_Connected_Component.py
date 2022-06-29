7cf704e331aa45bd7315bcc2c0ccfffee44db17c
Morteza Moradzadeh <47799003+mortezam037@users.noreply.github.com>
2022-06-29 20:49:24 +0430

    Update and rename S10C11.py to S10C11_Connected_Component.py

--- S10/S10C11_Connected_Component.py
+++ S10/S10C11_Connected_Component.py
@@ -1,18 +1,19 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+
 A = cv2.imread("TextC.bmp",0)
 B = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
 #B = cv2.getStructuringElement(cv2.MORPH_RECT,(20,20))
 Px,Py=90,20
 X_last=np.zeros(A.shape,dtype=np.uint8)
 X_last[Px,Py]=255
-
 X_current=cv2.morphologyEx(X_last, cv2.MORPH_DILATE, B) & A
 while (X_current != X_last).any():
     X_last=X_current
     X_current=cv2.morphologyEx(X_last, cv2.MORPH_DILATE, B) & A
 result=X_current
+# Visualize
 images=[A,result]
 titles=['Original','result']
 fig1 = plt.figure(figsize=(10, 10))
