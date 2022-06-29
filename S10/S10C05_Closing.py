b775fdadc879ebce9b243305b5facea0e6c7a9f8
Morteza Moradzadeh <47799003+mortezam037@users.noreply.github.com>
2022-06-29 19:53:48 +0430

    Update and rename S10C05.py to S10C05_Closing.py

--- S10/S10C05_Closing.py
+++ S10/S10C05_Closing.py
@@ -1,8 +1,11 @@
 import cv2
 import matplotlib.pyplot as plt
+ 
 img = cv2.imread("Test_C.BMP",0)
-SE = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))
-img1 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, SE)
+SE = cv2.getStructuringElement(cv2.MORPH_RECT,(5,5))    # Rectangular Kernel
+img1 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, SE)       # Closing
+
+# Visualize
 images=[img,img1]
 titles=['Orginal','Closing']
 fig1 = plt.figure(figsize=(10, 10))
