e87cd69cd978696dd6ba8c58ebce6f562c652d57
Morteza Moradzadeh <47799003+mortezam037@users.noreply.github.com>
2022-06-29 19:45:50 +0430

    Update and rename S10C01.py to S10C01_structuring_element.py

--- S10/S10C01_structuring_element.py
+++ S10/S10C01_structuring_element.py
@@ -1,15 +1,19 @@
 import cv2
 from skimage.morphology import square, rectangle, diamond, disk, octagon, star
 import matplotlib.pyplot as plt
-SE1=cv2.getStructuringElement(cv2.MORPH_RECT,(15,15))
-SE2=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
-SE3=cv2.getStructuringElement(cv2.MORPH_CROSS,(15,15))
-SE4=square(15)
-SE5=rectangle(15, 10)
-SE6=diamond(7)
-SE7=disk(7)
-SE8=octagon(7, 4)
-SE9=star(5)
+
+SE1=cv2.getStructuringElement(cv2.MORPH_RECT,(15,15))       # Rectangular Kernel with OpenCV
+SE2=cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))    # Elliptical Kernel with OpenCV
+SE3=cv2.getStructuringElement(cv2.MORPH_CROSS,(15,15))      # Cross-shaped Kernel with OpenCV
+
+SE4=square(15)          # square Kernel with skimage
+SE5=rectangle(15, 10)   # Rectangular Kernel with skimage
+SE6=diamond(7)          # diamond Kernel with skimage
+SE7=disk(7)             # disk Kernel with skimage
+SE8=octagon(7, 4)       # octagon Kernel with skimage
+SE9=star(5)             # star Kernel with skimage
+
+# Visualize the elements.
 SES=[SE1,SE2,SE3,SE4,SE5,SE6,SE7,SE8,SE9]
 titles=['CV2_rectangle','CV2_Elliptical','CV2_Cross-shaped','skimage_square','skimage_rectangle','skimage_diamond','skimage_disk','skimage_octagon','skimage_star']
 fig1 = plt.figure(figsize=(10, 10))
